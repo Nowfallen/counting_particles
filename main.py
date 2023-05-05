133492bf8c4bab9883f9234b75de9054e905abbe
naoufal elaisati <elaisatinaoufal@outlook.com>
2023-05-05 15:43:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ from msdmodule import averaging
 from msdmodule import total_average
 
 # parameters
-num_steps = 10
-number_particles = 99
-dt = 0.1
+num_steps =200
+number_particles = 100
+dt = 0.01
 c = 1
 #boundry 
 xmin=-10
 xmax=10
 ymin=-10
 ymax=10
-number_lignes=3
-number_sections=number_lignes**2
+number_lignes=2
+number_sections = int(math.pow(number_lignes, 2))
 #we will just have to consider it to be cubes for now
 section_length=(xmax-xmin)/number_lignes
 # Initializing steps 
@@ -35,21 +35,44 @@ for j in range(number_particles):
         # using Euler method to update the equation
         x[j, i+1] = (x[j, i] + np.sqrt(c * dt) * nx[j, i] - xmin) % (xmax - xmin) + xmin
         y[j, i+1] = (y[j, i] + np.sqrt(c * dt) * ny[j, i] - ymin) % (ymax - ymin) + ymin
-
-# Distribution of particles in every section according to time
 def Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes):
     num_particles_insection = np.zeros((number_lignes**2, num_steps))
-    for i_x in range(number_lignes):
-        for i_y in range(number_lignes):
-            for i in range(num_steps):
-                for j in range(number_particles):
-                    if (xmin + i_x * section_length < x[j, i] < xmin + (i_x + 1) * section_length) and \
-                       (ymin + i_y * section_length < y[j, i] < ymin + (i_y + 1) * section_length):
-                        num_particles_insection[i_x * number_lignes + i_y, i] += 1
+    for i in range(num_steps):
+        for j in range(number_particles):
+            xposition=int((x[j,i]-xmin)/section_length)
+            yposition=int((y[j,i]-ymin)/section_length)
+            num_particles_insection[xposition * number_lignes + yposition, i] += 1
     return num_particles_insection
 
+#  the distribution difference  
+def distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes):
+    number_sections = number_lignes**2
+    distribution_diff = np.zeros((number_sections, num_steps))
+    distribution_t0 = Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)[:,0]
+    for t in range(1, num_steps):
+        distribution_t = Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)[:,t-1]
+        distribution_diff[:,t] = (distribution_t - distribution_t0)**2
+    return distribution_diff
+diff = distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)
+# Plot the squared difference of particle distributions for each section
+for i in range(number_sections):
+    plt.plot(range(num_steps), diff[i])   
+#the msd that will average on all sections
+def computational_msd(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes):
+    computational_displacement = np.zeros(num_steps)
+    for t in range(num_steps):
+        computational_displacement[t] = np.mean(distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)[:, t])
+    return computational_displacement
+msd = computational_msd(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)
+# the mean squared displacement plot
+plt.plot(range(num_steps), msd,color="black")
+plt.grid()
+plt.show()
+  
 num_particles_insection = Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)
 
+
+"""
 # creating subplots for each section
 fig, axes = plt.subplots(number_lignes, number_lignes, figsize=(7 * number_lignes, 7 * number_lignes), sharey=True)
 for i_x in range(number_lignes):
@@ -68,7 +91,7 @@ fig.suptitle(f"Number of particles in each section through time\n"
              f"The average number of particles in all the sections: {round(total_average(num_particles_insection, num_steps),1)}")
 
 plt.show()
-"""
 #in case we wwant to animate it to show us the particles moving and it calls a modulus to do so
 #output_path = "/home/elaisati/2D animation.gif"
 #animate_particles(x, y, num_steps,number_particles, xmin, xmax, ymin, ymax, output_path) """
+

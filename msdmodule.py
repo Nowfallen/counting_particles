ed9ba6d1dd89c86d0bf1f341c1c98b60b95a1f93
naoufal elaisati <elaisatinaoufal@outlook.com>
2023-05-09 18:09:01 +0200

    Update msdmodule.py

--- msdmodule.py
+++ msdmodule.py
@@ -1,20 +1,35 @@
 import numpy as np
 import math
 
+#parameters
+D=0.1
+num_steps=100
 
-# the msd equation 2-2*correlation(i think i should add intial time so i can average over it):
-def msd(num_particles_insection, number_sections, t, section_length, D):
-    return 2 - 2 * cN(t, num_particles_insection, number_sections, section_length, D)
-
-
-
-
+#time the particle takes to diffuse in one section:
+def toudiff(section_length, D):
+    return (section_length ** 2) / (4 * D)
 
 #the correlation equation:
 def cN(t, num_particles_insection, number_sections, section_length, D):
-    return averaging(num_particles_insection, num_steps)* (np.sqrt(t/ (toudiff(section_length, D) * np.pi)) * (np.exp(-toudiff_value/t)- 1)) + math.erf(np.sqrt(toudiff(section_length, D) / t))
+    toudiff_value = toudiff(section_length, D)
+    mean_particles = np.mean(num_particles_insection)  # compute the mean across all particles and sections
+    return mean_particles * (np.sqrt(t/ (toudiff_value * np.pi)) * (np.exp(-toudiff_value/t)- 1)) + math.erf(np.sqrt(toudiff_value / t))
+
+# the msd equation 2-2*correlation:
+def msd_analytic(num_particles_insection, number_sections, num_steps, section_length, D):
+    msd_analytic = np.zeros(num_steps)
+    for t in range(1,num_steps):
+        msd_analytic[t] = 2 - 2 * cN(t, num_particles_insection, number_sections, section_length, D)
+    return msd_analytic
 
 
+
+
+
+
+
+
+"""
 #average number of particles in a particular section:
 def averaging(num_particles_insection, num_steps):
     num_sections = num_particles_insection.shape[0]
@@ -35,15 +50,12 @@ def computational_msd(number_particles, num_steps):
 #the total average :
 def total_average(num_particles_insection, num_steps):
     num_sections = num_particles_insection.shape[0]
-    return np.sum(averaging(num_particles_insection,num_steps))/num_sections
+    return np.sum(averaging(num_particles_insection,num_steps))/num_sections """
 
  
-#time the particle takes to diffuse in one section:
-def toudiff(section_length, D):
-    return (section_length ** 2) / (4 * D)
 
-def distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes):
-    number_sections = number_lignes**2
+
+
     distribution_diff = np.zeros((number_sections, num_steps))
     distribution_t0 = Distribution(x, y, xmin, ymin, section_length, 1, number_particles, number_lignes)
     for t in range(1, num_steps):

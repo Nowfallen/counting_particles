bf18160c0637763ae7cdd0a330bee10436cd4331
naoufal elaisati <elaisatinaoufal@outlook.com>
2023-05-12 15:16:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,22 @@ from msdmodule import msd_analytic
 import pickle
 
 # parameters
-num_steps =10000
+num_steps =1000
+n=1
 number_particles = 1000
 #boundry 
 xmin=0
 xmax=20
 ymin=0
 ymax=20
-number_lignes=4
+number_lignes=25
 D=0.5
 number_sections = int(math.pow(number_lignes, 2))
+pmax=int(num_steps/n)
 #we will just have to consider it to be cubes for now
 section_length=(xmax-xmin)/number_lignes
+meanParticles=number_particles/number_sections
+tauDiff= ((xmax-xmin)**2)/4*D
 
 #opening the pickle file 
 with open('particle_positions1.pkl', 'rb') as f:
@@ -29,8 +33,8 @@ def Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, 
     num_particles_insection = np.zeros((number_lignes**2, num_steps))
     for i in range(num_steps):
         for j in range(number_particles):
-            xposition=int((x[j,i]-xmin)/section_length)
-            yposition=int((y[j,i]-ymin)/section_length)
+            xposition = int((x[j, i]) / section_length)
+            yposition = int((y[j, i]) / section_length)
             num_particles_insection[xposition * number_lignes + yposition, i] += 1
     return num_particles_insection
 num_particles_insection = Distribution(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)
@@ -38,46 +42,38 @@ num_particles_insection = Distribution(x, y, xmin, ymin, section_length, num_ste
 #  the distribution difference  sqr
 def distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes):
     distribution_diff = np.zeros((number_sections, num_steps))
-    for t in range(1, num_steps):
-        distribution_t = num_particles_insection[:,t-1] 
-        #the code 
-        for k in range (1,t-1):
-                  distribution_t0=num_particles_insection[:,k]
-                  distribution_diff[:,t] = (distribution_t-distribution_t0)**2
+    for delta in range(1, num_steps): 
+        for t0 in range (1, num_steps): 
+            distribution_t0 = num_particles_insection[:,t0]
+            if t0 + n*delta < num_particles_insection.shape[1]:
+                distribution_t = num_particles_insection[:,t0 + delta]
+            distribution_diff[:,delta] += (distribution_t - distribution_t0)**2
+        distribution_diff[:,delta] /= pmax
+    return distribution_diff
+
     return distribution_diff
 diff = distribution_diff(x, y, xmin, ymin, section_length, num_steps, number_particles, number_lignes)
 #plotting it
-"""
+
 for i in range(number_sections):
-    plt.plot(range(num_steps), diff[i]) """
+    plt.plot(range(num_steps), diff[i]) 
 #the msd that will average on all sections
 def computational_msd(diff, num_steps):
     computational_displacement = np.zeros(num_steps)
     for t in range(num_steps):
-        for k in range(t-1):
-            computational_displacement[t] += np.mean(diff[:, k])
-        computational_displacement[t]/= t
-    return computational_displacement
-"""
-def computational_msd(diff, num_steps):
-    computational_displacement = np.zeros(num_steps)
-    for t in range(num_steps):
-        for k in range(t): 
-            computational_displacement[k]=np.mean(diff[:,t])
         computational_displacement[t] = np.mean(diff[:, t])
-        
     return computational_displacement
-"""
 msd = computational_msd(diff, num_steps)
-msd_anly=msd_analytic( number_sections, num_steps, section_length, D)
+msd_anly=msd_analytic( meanParticles,tauDiff,number_sections, num_steps, section_length, D)
 
-# Plot the msd
-plt.loglog(range(num_steps), msd,color="black",label="the mean")
-plt.loglog(range(num_steps),msd_anly,color='blue',label='analytic mean')
+time = np.arange(num_steps)
+rescaled_timesteps = time * tauDiff
+plt.loglog(range(num_steps), msd, '+', color="black", label="the mean")
+plt.loglog(range(num_steps), msd_anly, color='blue', label='analytic mean')
 plt.grid()
 plt.legend()
-plt.xlabel("number of steps")
-plt.ylabel("displacement sqr value of distribution ")
+plt.xlabel("time ")
+plt.ylabel("number of flactuations")
 plt.title('msd :distribution of particles in the observed space', color='blue')
 plt.show()
   
@@ -105,5 +101,32 @@ plt.show()
 
 #in case we wwant to animate it to show us the particles moving and it calls a modulus to do so
 #output_path = "/home/elaisati/2D animation.gif"
-#animate_particles(x, y, num_steps,number_particles, xmin, xmax, ymin, ymax, output_path) """
+  
+
+
+"""
+# creating subplots for each section
+fig, axes = plt.subplots(number_lignes, number_lignes, figsize=(7 * number_lignes, 7 * number_lignes), sharey=True)
+for i_x in range(number_lignes):
+    for i_y in range(number_lignes):
+       
+                  k = i_x * number_lignes + i_y
+                  axes[i_x, i_y].bar(range(num_steps), num_particles_insection[k])
+                  axes[i_x, i_y].set_xlabel("Number of steps")
+                  axes[i_x, i_y].set_ylabel("Number of particles")
+                  axes[i_x, i_y].set_title(f"xaxis {round(xmin + i_x * section_length,1) ,round(xmin + (i_x + 1) * section_length,1) } and yaxis {round(ymin + i_y * section_length,1) ,round(ymin + (i_y + 1) * section_length,1)}\n the average number of particles :{averaging(num_particles_insection,num_steps)[k]} ",color="blue")
+                  axes[i_x, i_y].grid(color="Grey", linewidth="0.5", linestyle="-.")
+                 
+
+
+fig.suptitle(f"Number of particles in each section through time\n"
+             f"The average number of particles in all the sections: {round(total_average(num_particles_insection, num_steps),1)}")
+
+plt.show()
+
+#in case we wwant to animate it to show us the particles moving and it calls a modulus to do so
+#output_path = "/home/elaisati/2D animation.gif"
+#animate_particles(x, y, num_steps,number_particles, xmin, xmax, ymin, ymax, output_path) 
+
+"""
 

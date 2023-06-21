198f5fa4bae0223b6443f6f2a90ec53b3bf9d6db
naoufal elaisati <elaisatinaoufal@outlook.com>
2023-06-21 19:05:47 +0200

    Update mainmodule.py

--- mainmodule.py
+++ mainmodule.py
@@ -15,17 +15,17 @@ def Distribution_data(x, y,data, section_lengthx,section_lengthy, num_steps, num
 
 
 
-def distribution_diff(x, y, section_lengthx,section_lengthy, num_steps, number_lignes,number_sections,num_particles_insection_data):
-    distribution_diff = np.zeros((number_sections, num_steps))
+def fluctuation_diff(x, y, section_lengthx,section_lengthy, num_steps, number_lignes,number_sections,num_particles_insection_data):
+    fluctuation_diff = np.zeros((number_sections, num_steps))
     for delta in range(1,num_steps):
         nt0 = 0
         for t0 in range (0, num_steps-delta):
-            distribution_t0 = num_particles_insection_data[:,t0]
-            distribution_t = num_particles_insection_data[:,t0 + delta]
-            distribution_diff[:,delta] += (distribution_t - distribution_t0)**2
+            fluctuation_t0 = num_particles_insection_data[:,t0]
+            fluctuation_t = num_particles_insection_data[:,t0 + delta]
+            fluctuation_diff[:,delta] += (distribution_t - distribution_t0)**2
             nt0 += 1
-        distribution_diff[:,delta] /= nt0
-    return distribution_diff
+        fluctuation_diff[:,delta] /= nt0
+    return fluctuation_diff
 
 def computational_msd(diff, num_steps):
     computational_displacement = np.zeros(num_steps)
